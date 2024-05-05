b101ebea055345aad07455b123905327d3a52766
jackestes06 <jackestes3210@gmail.com>
2024-05-05 10:15:40 -0600

    Learning memory - Your pokemon in battle

--- pyboy_test.py
+++ pyboy_test.py
@@ -1,9 +1,11 @@
 from pyboy import PyBoy
 
 pyboy = PyBoy('Pokemon - Gold Version (UE) [C][!].gbc', sound=True)
-while pyboy.tick():
-    print(f"events: {pyboy.events}")
-    print(f"memory: {pyboy.memory[0xCB0D:0xCBC1]}")
-    print(f"memory val: {pyboy.get_memory_value()}")
+while pyboy.tick(60, True):
+    print("Checking for events")
+    if pyboy.events:
+        print(f"events: {pyboy.events}")
+    # print(f"memory: {pyboy.memory[0xCB0D:0xCBC1]}")
+    # print(f"memory val: {pyboy.get_memory_value()}")
     pass
 pyboy.stop()
