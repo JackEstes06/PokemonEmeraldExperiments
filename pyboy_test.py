e9673822c6a0ea419d41feeed4addb4a4a2b7777
jackestes06 <jackestes3210@gmail.com>
2024-05-05 20:48:14 -0600

    More memory setups - Items, Moves, PPMoves, HP, Type

--- pyboy_test.py
+++ pyboy_test.py
@@ -1,48 +1,43 @@
 from pyboy import PyBoy
 from ramMapConstants import *
 
-pyboy = PyBoy('Pokemon - Gold Version (UE) [C][!].gbc', sound=True)
+pyboy = PyBoy('Pokemon - Gold Version (UE) [C][!].gbc', sound=True, scale=3)
 previousStats = ""
 currentStats = ""
+pyboy.set_emulation_speed(4)
 
 while pyboy.tick():
     if pyboy.events:
         print(f"events: {pyboy.events}")
 
     itemHeldMemory: int = pyboy.memory[0xCB0D]
-    movesMemory = pyboy.memory[0xCB0E:0xCB11]
-    ppMovesMemory = pyboy.memory[0xCB14:0xCB17]
+    movesMemory: list[:int] = pyboy.memory[0xCB0E:0xCB12]
+    ppMovesMemory: list[:int] = pyboy.memory[0xCB14:0xCB18]
+    # TODO: Figure out if this actually works
     statusMemory = pyboy.memory[0xCB1A]
-    hpMemory = pyboy.memory[0xCB1C:0xCB1D]
-    typeMemory = pyboy.memory[0xCB2A:0xCB2B]
+    # end TODO
+    hpMemory = pyboy.memory[0xCB1C:0xCB1E]
+    typeMemory = pyboy.memory[0xCB2A:0xCB2C]
     substituteMemory = pyboy.memory[0xCB49]
+    # TODO: These aren't working -> need to find the memory locations proper
     moneyMemory = pyboy.memory[0xCB65:0xCB66]
     expGivenMemory = pyboy.memory[0xCB7E:0xCB7F]
+    # end TODO
     currAttMemory = pyboy.memory[0xCBC1]
-    currentStats = f"Item held memory: {itemHeldMemory} -> {itemConstants[itemHeldMemory]}\nMoves 1-4 memory: {movesMemory}\nPP Moves 1-4 memory: {ppMovesMemory}\nStatus memory: {statusMemory}\nHP in Battle memory: {hpMemory}\nType 1-2 memory: {typeMemory}\nSubstitute memory: {substituteMemory}\nMoney earned memory: {moneyMemory}\nExp Given memory: {expGivenMemory}\nCurrent Attack memory: {currAttMemory}\n\n\n"
-
-    # print(currentStats)
+    
+    currentStats = (f"Item held memory: {itemHeldMemory} -> {itemConstants[itemHeldMemory]}\n"
+                    f"Moves 1-4 memory: {movesMemory} -> {printMoves(movesMemory)}\n"
+                    f"PP Moves 1-4 memory: {ppMovesMemory}\n"
+                    f"Status memory: {statusMemory}\n"
+                    f"HP in Battle memory: {hpMemory}\n"
+                    f"Type 1-2 memory: {typeMemory}\n"
+                    f"Substitute memory: {substituteMemory}\n"
+                    f"Money earned memory: {moneyMemory}\n"
+                    f"Exp Given memory: {expGivenMemory}\n"
+                    f"Current Attack memory: {currAttMemory}\n\n\n")
 
     if currentStats != previousStats: 
         previousStats = currentStats
         print(previousStats)
-
-    # print(f"Item held memory: {pyboy.memory[0xCB0D]}")
-    # print(f"Moves 1-4 memory: {pyboy.memory[0xCB0E:0xCB11]}")
-    # print(f"PP Moves 1-4 memory: {pyboy.memory[0xCB14:0xCB17]}")
-    # print(f"Status memory: {pyboy.memory[0xCB1A]}")
-    # # Skip 0XCB1B
-    # print(f"HP in Battle memory: {pyboy.memory[0xCB1C:0xCB1D]}")
-    # print(f"Type 1-2 memory: {pyboy.memory[0xCB2A:0xCB2B]}")
-    # # Skip 0xCB2C:0xCB48
-    # print(f"Substitute memory: {pyboy.memory[0xCB49]}")
-    # # Skip 0xCB4A:0xCB64
-    # print(f"Money earned memory: {pyboy.memory[0xCB65:0xCB66]}")
-    # # Skip 0xCB67:0xCB7D
-    # print(f"Exp Given memory: {pyboy.memory[0xCB7E:0xCB7F]}")
-    # # Skip 0xCB80:0xCBC0
-    # print(f"Current Attack memory: {pyboy.memory[0xCBC1]}")
-    # print("\n\n")
-    # # print(f"memory val: {pyboy.get_memory_value()}")
     pass
 pyboy.stop()
